3e4d40a0e03afb9790dcfac1f52f71c28172dc39
Advait Bhitkar <advaitbhitkar@gmail.com>
2025-03-30 12:25:42 +0530

    fixed suspense

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react"
 import { ResetPasswordForm } from "@/components/reset-password-form"
 
 export default function ResetPasswordPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <ResetPasswordForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ResetPasswordForm />
+        </Suspense>
       </div>
     </div>
   )
